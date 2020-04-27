cba8d3101c22243a582f02a54007c0d5153a6980
Samson <kempiak@gmail.com>
2020-04-27 13:25:48 -0700

    DoNe

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -10,56 +10,71 @@ private int[] values;
 	StringIntegerPair() {
 		// 3. initialize both member arrays to a length of 0
 		values = new int[0];
+		keys = new String[0];
 	}
 
 	// 4. Complete the steps in the put method
 	public void put(String key, int value) {
-		// A. iterate through the keys. If you find a key that matches
-		// the passed in String, set the value at that location to the
-		// passed in value and return from the method.
-
+		// A. iterate through the keys. If you find a key that matches the passed in String, set the value at that location to the passed in value and return from the method.
+for (int i = 0; i < keys.length; i++) {
+	if (keys[i].equals(key)) {
+		values[i] = value;
+	}
+}
 		// B. create a String array that is one element longer than the keys
-
+String[] keyMore = new String[keys.length + 1];
 		// C. create an integer array that is one element longer than values
-
+int[] valueMore = new int[values.length + 1];
 		// D. set the last element of the new String array to the passed in key
-
+keyMore[keyMore.length - 1] = key;
 		// E. set the last element of the new int array to the passed in value
-
-		// F. iterate through the keys and values and copy the elements to the new
-		// arrays
-
-		// G. Set the keys and values arrays equal to the new arrays created in steps B
-		// and C.
+valueMore[valueMore.length - 1] = value;
+		// F. iterate through the keys and values and copy the elements to the new arrays
+for (int i = 0; i < keys.length; i++) {
+		keyMore[i] = keys[i];
+}
+for (int i = 0; i < values.length; i++) {
+	valueMore[i] = values[i];
+}
+		// G. Set the keys and values arrays equal to the new arrays created in steps B and C.
+keys = keyMore;
+values = valueMore;
 	}
-
-	// 5. Complete the method so it returns the value located at the passed in key.
-	// If the key does not exist, return Integer.MIN_VALUE.
+	// 5. Complete the method so it returns the value located at the passed in key. If the key does not exist, return Integer.MIN_VALUE.
 	public int get(String key) {
-		return 0;
+	for (int j = 0; j < keys.length; j++) {
+		if (keys[j].equals(key)) {
+			return values[j];
+		}
 	}
-
-	// 6. Complete the containsKey method so that it returns true if the
-	// passed in keys is contained in the keys array
+return Integer.MIN_VALUE;
+	}
+	// 6. Complete the containsKey method so that it returns true if the passed in keys is contained in the keys array
 	public boolean containsKey(String key) {
-
-		return false;
+		for (int j = 0; j < keys.length; j++) {
+			if (keys[j].equals(key)) {
+				return true;
+			}
+		}
+	return false;
 	}
-
-	// 7. Complete the containsValue method so that it returns true if the
-	// passed in value is contained in the values array
+	// 7. Complete the containsValue method so that it returns true if the passed in value is contained in the values array
 	public boolean containsValue(int value) {
-
-		return false;
+		for (int j = 0; j < keys.length; j++) {
+			if (values[j] == value) {
+				return true;
+			}
+		}
+		return false;	
 	}
-
 	// 8. Complete the getKeysMethod so it returns the keys as an array
 	public String[] getKeys() {
-		return null;
+		String[] keyArray = keys;
+		return keyArray;
 	}
-
 	// 9. Complete the getValues so it returns the values as an array
 	public int[] getValues() {
-		return null;
+		int[] valueArray = values;
+		return valueArray;
 	}
 }
--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,29 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton("Add an Entry");
+	JButton button2 = new JButton("Find an Entry");
+	JButton button3 = new JButton("View all Entries");
+	String entries = "";
+	HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
+	int idNum = 0;
+	String name = "";
+	public static void main(String[] args) {
+		_02_LogSearch ls = new _02_LogSearch();
+		ls.start();
+	}
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -9,25 +31,71 @@ public class _02_LogSearch {
 	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
 	 * 				After an ID is entered, use another input dialog to ask the user to enter a name.
 	 * 				Add this information as a new entry to your HashMap.
-	 * 
-	 * Button 2: Search by ID
+	 */
+	void addEntry() {
+		String idNumS = JOptionPane.showInputDialog("What is your id Number?");
+		idNum = Integer.parseInt(idNumS);
+		name = JOptionPane.showInputDialog("What is your name");
+		hashMap.put(idNum, name);
+	}
+	 /* Button 2: Search by ID
 	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
 	 * 				If that ID exists, display that name to the user.
 	 * 				Otherwise, tell the user that that entry does not exist.
-	 * 
-	 * Button 3: View List
+	 */ 
+	 void findEntry() {
+		 String idNumS = JOptionPane.showInputDialog("What is the id Number of the person you are looking for?");
+		 idNum = Integer.parseInt(idNumS);
+		for (Integer key : hashMap.keySet()) {
+			if (key == idNum) {
+				JOptionPane.showMessageDialog(null, "The persons name is: " + hashMap.get(idNum));
+				break;
+			}
+		}
+	 }
+	/* Button 3: View List
 	 * 				When this button is clicked, display the entire list in a message dialog in the following format:
 	 * 				ID: 123  Name: Harry Howard
 	 * 				ID: 245  Name: Polly Powers
 	 * 				ID: 433  Name: Oliver Ortega
 	 * 				etc...
-	 * 
-	 * When this is complete, add a fourth button to your window.
+	*/ 
+	 void showEntries() {
+		 JOptionPane.showMessageDialog(null, "Here are the entries.");
+		 for (Integer key : hashMap.keySet()) {
+			 entries = entries + "\n ID: " + key + " Name: " + hashMap.get(key);
+		}
+		 JOptionPane.showMessageDialog(null, entries);
+	 }
+	 /* When this is complete, add a fourth button to your window.
 	 * Button 4: Remove Entry
 	 * 				When this button is clicked, prompt the user to enter an ID using an input dialog.
 	 * 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
 	 * 				is not in the list. 
-	 *
-	 * */
+	 */
+	private void start() {
+		// TODO Auto-generated method stub
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	if (e.getSource() == button) {
+		addEntry();
+	}else if (e.getSource() == button2) {
+		findEntry();
+	}else if (e.getSource() == button3) {
+		showEntries();
+	}
+	}
 	
 }
